a879eae7ad9fdd10c39d92dfeed64f896e4b4ffb
Bastien Decorte <bastiendct@gmail.com>
2020-01-15 11:49:21 +0100

    Updated FormerStudent approval methods.

--- src/main/java/be/helha/aemt/dao/MemberDAO.java
+++ src/main/java/be/helha/aemt/dao/MemberDAO.java
@@ -8,6 +8,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
+import be.helha.aemt.entity.FormerStudent;
 import be.helha.aemt.entity.Member;
 import be.helha.aemt.helper.Config;
 
@@ -16,20 +17,31 @@ import be.helha.aemt.helper.Config;
 public class MemberDAO {
 	@PersistenceContext(unitName = Config.UNIT_NAME)
 	private EntityManager em;
-	
+
 	public List<Member> selectAll() {
 		Query selectAll = em.createNamedQuery("Member.queryAll");
 		return selectAll.getResultList();
 	}
 
 	public Member add(Member m) {
-//			if (find(u) == null) {
-		em.persist(m);
-//			}
+		String username = m.getUsername();
+		if (findIdByUsername(username) < 0) {
+			em.persist(m);
+			int id = findIdByUsername(username);
+			m.setIdMember(id);
+			return m;
+		}
 
 		return null;
 	}
-	
+
+	public int findIdByUsername(String username) {
+		Query selectU = em.createNamedQuery("Member.queryIdByUsername");
+		selectU.setParameter("username", username);
+		int id = (int) selectU.getResultList().get(0);
+		return id;
+	}
+
 	public Member findByUsername(String username) {
 		Query selectU = em.createNamedQuery("Member.queryByUsername");
 		selectU.setParameter("username", username);
@@ -37,24 +49,29 @@ public class MemberDAO {
 
 		return tmp.size() == 0 ? null : tmp.get(0);
 	}
-	
-	public boolean updateFormerApproved(int id, boolean approved) {
-		Query query = em.createNamedQuery("FormerStudent.updateApproved");
-		query.setParameter("approved", approved);
-		query.setParameter("id", id);
+
+	public boolean updateFormerApproved(String username, boolean approved) {
+		Member m = findByUsername(username);
+		if (m instanceof FormerStudent) {
+			FormerStudent fs = (FormerStudent) m;
+			if(fs.isApproved() == approved) {
+				fs.setApproved(approved);
+				FormerStudent fsMerged = em.merge(fs);
+				return fs.isApproved() != fsMerged.isApproved();
+			} else {
+				throw new IllegalArgumentException("FormerStudent is already " + ((approved)?"approved":"unapproved"));
+			}
+			
+		} 
 		
-		
-		if(query.executeUpdate() > 0) {
-			return true;
-		}
-		return false;
+		throw new IllegalArgumentException("Given username not related to a FormerStudent.");
 	}
-	
+
 	public Member queryByPortrait(int idPortrait) {
 		Query query = em.createNamedQuery("FormerStudent.queryByPortrait");
 		query.setParameter("id", idPortrait);
 		List<Member> results = query.getResultList();
-		if(results.size() > 0) {
+		if (results.size() > 0) {
 			return results.get(0);
 		}
 		return null;
--- src/main/java/be/helha/aemt/ejb/MemberManagerEJB.java
+++ src/main/java/be/helha/aemt/ejb/MemberManagerEJB.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.persistence.Query;
 
 import be.helha.aemt.dao.MemberDAO;
 import be.helha.aemt.entity.Member;
@@ -26,8 +27,12 @@ public class MemberManagerEJB {
 		return dao.findByUsername(username);
 	}
 	
-	public boolean updateFormerApproved(int id, boolean approved) {
-		return dao.updateFormerApproved(id, approved);
+	public int findIdByUsername(String username) {
+		return dao.findIdByUsername(username);
+	}
+	
+	public boolean updateFormerApproved(String username, boolean approved) {
+		return dao.updateFormerApproved(username, approved);
 	}
 	
 	public Member queryByPortrait(int idPortrait) {
